52b991037141c378f734ce8f0705e7e723a92151
StevenYAMBOS <stevenyambos@gmail.com>
2025-09-08 22:48:07 +0200

    Connexion PostgreSQL

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
 import bodyParser from "body-parser";
-import express, { Response } from "express";
-import { neon } from "@neondatabase/serverless";
+import express, { Request, Response } from "express";
+// import cors from "cors";
 import "dotenv/config";
+import { pool } from "../database/database";
 
 const app = express();
-const port = process.env.PORT;
+const PORT = process.env.PORT;
 
+// app.use(cors);
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", async (_, res: Response) => {
-  const sql = neon(`${process.env.DATABASE_URL}`);
-  const response = await sql`SELECT version()`;
-  const { version } = response[0];
-  res.json({ version });
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello world");
 });
 
-app.listen(port, () => {
-  console.log(`API lançée à l'adresse : http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`API lançée à l'adresse : http://localhost:${PORT}`);
+  console.log(`Connexion à la base de données : `, pool);
 });

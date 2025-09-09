16169bed5836a3dc0625f038bd64027a623a142b
Steven YAMBOS <stevenyambos@gmail.com>
2025-09-09 12:21:59 +0200

    feat : Setup base de données
    - Correction diverses de Postgres sur MacOS (j'ai galéré...)
    - Connexion à la base de données fonctionnelle
    - Variables d'environnement configurées

--- internal/database/database.go
+++ internal/database/database.go
@@ -10,22 +10,20 @@ import (
 	_ "github.com/lib/pq"
 )
 
-var (
-	host     = os.Getenv("DB_HOST")
-	port     = os.Getenv("DB_PORT")
-	user     = os.Getenv("DB_USER")
-	password = os.Getenv("DB_PASSWORD")
-	dbname   = os.Getenv("DB_NAME")
-)
-
 func InitDB() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal(`[database.go] -> Erreur lors du chargement des variables d'environnements.`)
 	}
 
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
 	// connection string
-	psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+	psqlconn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 
 	// open database
 	db, err := sql.Open("postgres", psqlconn)
@@ -38,7 +36,7 @@ func InitDB() {
 	err = db.Ping()
 	CheckError(err)
 
-	fmt.Println("Connected!")
+	fmt.Println(`[database.go] -> Connexion à la base de données réussie !`)
 }
 
 func CheckError(err error) {

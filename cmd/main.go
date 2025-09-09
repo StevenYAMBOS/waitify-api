f7c29c87634359367599f1cbefb9606824cecf03
Steven YAMBOS <stevenyambos@gmail.com>
2025-09-09 10:46:41 +0200

    fix : Nouvelle stack
    MVP développé en Golang

---
+++ cmd/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal(`[main.go] -> Erreur lors du chargement des variables d'environnements.`)
+	}
+
+	port := os.Getenv("PORT")
+
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Health check !"))
+	})
+	fmt.Print("[main.go] -> Serveur lançé : http://localhost", port)
+	http.ListenAndServe(port, r)
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/StevenYAMBOS/waitify-api
+
+go 1.24.3
+
+require (
+	github.com/go-chi/chi/v5 v5.2.3 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)

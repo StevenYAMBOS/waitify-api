8572efce03b00b6445c2eb61193ac0fec6d2ac7b
StevenYAMBOS <stevenyambos@gmail.com>
2025-09-30 11:18:12 +0200

    Refactor du projet en Java

---
+++ src/main/java/com/stevenyambos/waitify_api/WaitifyApiApplication.java
@@ -0,0 +1,13 @@
+package com.stevenyambos.waitify_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaitifyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaitifyApiApplication.class, args);
+	}
+
+}

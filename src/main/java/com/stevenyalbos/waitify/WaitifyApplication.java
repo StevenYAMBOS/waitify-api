6758552b48f4aacc61e8feafa39bebc6e2a2ef1f
StevenYAMBOS <stevenyambos@gmail.com>
2025-10-01 15:09:18 +0200

    Branche propre

---
+++ src/main/java/com/stevenyalbos/waitify/WaitifyApplication.java
@@ -0,0 +1,14 @@
+package com.stevenyalbos.waitify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+public class WaitifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaitifyApplication.class, args);
+	}
+
+}

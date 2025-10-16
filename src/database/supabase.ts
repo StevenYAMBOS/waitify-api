be2e3e016782954054efc760b320bf9d8b5095f9
StevenYAMBOS <stevenyambos@gmail.com>
2025-10-16 17:32:38 +0200

    feat : Ajout du BodyParser
    Ajout d'un BodyParser à la racine du projet

--- src/database/supabase.ts
+++ src/database/supabase.ts
@@ -2,4 +2,4 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseURL = process.env.SUPABASE_URL;
 const supabaseAPIKey = process.env.SUPABASE_API_KEY;
-const supabase = createClient(supabaseURL, supabaseAPIKey);
+export const supabase = createClient(supabaseURL, supabaseAPIKey);

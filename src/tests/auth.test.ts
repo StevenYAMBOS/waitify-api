82982b95653d3d75a30ec756a7f6ef48ff869939
StevenYAMBOS <stevenyambos@gmail.com>
2025-11-20 23:01:57 +0100

    fix : Suppression anciennes constantes #40

--- src/tests/auth.test.ts
+++ src/tests/auth.test.ts
@@ -2,26 +2,23 @@ import request from "supertest";
 import app from "../server";
 import { LoginRequest } from "../auth/models/authModels";
 import {
-  AUTH_PATH,
-  GET_METHOD,
-  LOGIN_PATH,
-  OK,
-  TEST_LOGIN_EMAIL,
-  TEST_LOGIN_PASSWORD,
-  TEST_LOGIN_RULE_MESSAGE,
+  HTTP_METHODS,
+  HTTP_STATUS,
+  ROUTES_AUTH,
+  TEST_DATA,
 } from "../config/constants";
 
 // Requête test pour connecter l'utilisateur
 const loginRequest: LoginRequest = {
-  email: TEST_LOGIN_EMAIL,
-  password: TEST_LOGIN_PASSWORD,
+  email: TEST_DATA.TEST_USER_EMAIL,
+  password: TEST_DATA.TEST_USER_PASSWORD,
 };
 
-describe(`${GET_METHOD + AUTH_PATH + LOGIN_PATH}`, () => {
-  it(`${TEST_LOGIN_RULE_MESSAGE}`, async () => {
+describe(`${HTTP_METHODS.GET} ${ROUTES_AUTH.BASE}${ROUTES_AUTH.LOGIN}`, () => {
+  it(`${TEST_DATA.TEST_LOGIN_SHOULD_SUCCEED}`, async () => {
     return request(app)
-      .post(`${AUTH_PATH + LOGIN_PATH}`)
+      .post(`${ROUTES_AUTH.BASE}${ROUTES_AUTH.LOGIN}`)
       .send(loginRequest)
-      .expect(OK);
+      .expect(HTTP_STATUS.OK);
   });
 });

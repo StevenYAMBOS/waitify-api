7785aba4cf3cd98c2a139aa08f0429bfcaffebdf
StevenYAMBOS <stevenyambos@gmail.com>
2026-09-25 22:19:36 +0200

    update : QueueOpenedAt NULL.
    Passage de 'QueueOpenedAt TIMESTAMP WITH TIME ZONE DEFAULT NOW()' à 'TIMESTAMP WITH TIME ZONE (table Businesses)'.
    En gros passer de NOT NULL à NULL.

---
+++ src/Migrations/20260925201248_QueueOpenedAtToNull.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WaitifyApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class QueueOpenedAtToNull : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ServedAt",
+                table: "QueueEntries",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "QueueOpeneddAt",
+                table: "Businesses",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QueueEntries_BusinessQrCodeToken_Status",
+                table: "QueueEntries",
+                columns: new[] { "BusinessQrCodeToken", "Status" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_QueueEntries_BusinessQrCodeToken_Status",
+                table: "QueueEntries");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ServedAt",
+                table: "QueueEntries",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "QueueOpeneddAt",
+                table: "Businesses",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+        }
+    }
+}
--- src/Services/AnalyticsService.cs
+++ src/Services/AnalyticsService.cs
@@ -19,68 +19,75 @@ public class AnalyticsService(AppDbContext context, IApplicationUserRepository u
 {
     public async Task<GetBusinessLiveKpisResponse> GetLiveKpisAsync(Guid businessQrCodeToken, string userId)
     {
-        var user = await userService.FindUserByIdAsync(userId);
-        var business = await businessService.FindBusinessByQrTokenAsync(businessQrCodeToken);
-        DateTime dateTimeNow = DateTime.UtcNow.Date;
-        var dateTimeNowFormat = dateTimeNow.GetDateTimeFormats();
-
-        if (user == null)
+        try
         {
-            logger.LogError("[ERREUR] Utilisateur non récupéré : {@0}", user.Id);
-            throw new UnauthorizedAccessException("Accès interdit");
+            var user = await userService.FindUserByIdAsync(userId);
+            var business = await businessService.FindBusinessByQrTokenAsync(businessQrCodeToken);
+            DateTime dateTimeNow = DateTime.UtcNow.Date;
+            var dateTimeNowFormat = dateTimeNow.GetDateTimeFormats();
+
+            if (user == null)
+            {
+                logger.LogError("[ERREUR] Utilisateur non récupéré : {@0}", user.Id);
+                throw new UnauthorizedAccessException("Accès interdit");
+            }
+
+            Guid existingUserId = Guid.Parse(user.Id);
+
+            if (business == null)
+            {
+                logger.LogError("[ERREUR] Entreprise non récupérée : {@0}", user.Id);
+                throw new KeyNotFoundException("Entreprise non trouvée.");
+            }
+
+            var existingBusinessId = Guid.Parse(business.Owner.Id);
+
+            if (existingBusinessId != existingUserId)
+            {
+                logger.LogError("[ERREUR] Accès interdit, l'utilisateur n'est pas le gérant : \n `business.Owner` = '{@0}', \n `user.id` = '{@1}'", existingBusinessId, existingUserId);
+                throw new UnauthorizedAccessException("Accès interdit");
+            }
+
+            // Reproduire cette requête SELECT COUNT("Status") FROM "QueueEntries" WHERE "BusinessQrCodeToken" = '' AND "Status" = 'waiting';
+            int clientsWaiting = await context.Queues
+            .Where(q =>
+                q.BusinessQrCodeToken == businessQrCodeToken &&
+                q.Status == AppConstants.Queues.Status.Waiting)
+            .Select(q => q.Status)
+            .CountAsync();
+
+            // SELECT COUNT("Status") FROM "QueueEntries" WHERE "BusinessQrCodeToken" = '' AND "Status" = 'served' AND "ServedAt" = CURRENT_DATE;
+            int clientsServedToday = await context.Queues
+            .Where(q =>
+                q.BusinessQrCodeToken == businessQrCodeToken &&
+                q.Status == AppConstants.Queues.Status.Served &&
+                q.ServedAt == dateTimeNow)
+            .Select(q => q.Status)
+            .CountAsync();
+
+            // var averageWaitMinutes = context.Queues.FromSql($"SELECT ROUND(AVG(EstimatedWaitTime), 1) FROM QueueEntries WHERE BusinessQrCodeToken = {businessQrCodeToken}");
+            var averageWaitMinutes = Math.Round(context.Queues
+                .Where(q => q.BusinessQrCodeToken == businessQrCodeToken)
+                .Select(q => q.EstimatedWaitTime)
+                .Average(), 1);
+
+            var response = new GetBusinessLiveKpisResponse
+            {
+                BusinessQrCodeToken = business.QrCodeToken,
+                UpdatedAt = business.UpdatedAt,
+                ClientsWaiting = clientsWaiting,
+                ClientsServedToday = clientsServedToday,
+                AverageWaitMinutes = averageWaitMinutes,
+                QueueOpenSince = dateTimeNow
+            };
+
+            logger.LogInformation("[LOG] KPIS : {@0}", response);
+
+            return response;
+        } catch (Exception exeption)
+        {
+            logger.LogError($"[ERROR] Une erreur est survenue lors de la récupération des KPIs en temps réel : {exeption}");
+            throw new InvalidOperationException("Une erreur est survenue lors de la récupération des KPIs en temps réel.", exeption);
         }
-
-        Guid existingUserId = Guid.Parse(user.Id);
-
-        if (business == null)
-        {
-            logger.LogError("[ERREUR] Entreprise non récupérée : {@0}", user.Id);
-            throw new KeyNotFoundException("Entreprise non trouvée.");
-        }
-
-        var existingBusinessId = Guid.Parse(business.Owner.Id);
-
-        if (existingBusinessId != existingUserId)
-        {
-            logger.LogError("[ERREUR] Accès interdit, l'utilisateur n'est pas le gérant : \n `business.Owner` = '{@0}', \n `user.id` = '{@1}'", existingBusinessId, existingUserId);
-            throw new UnauthorizedAccessException("Accès interdit");
-        }
-
-        // Reproduire cette requête SELECT COUNT("Status") FROM "QueueEntries" WHERE "BusinessQrCodeToken" = '' AND "Status" = 'waiting';
-        int clientsWaiting = await context.Queues
-        .Where(q =>
-            q.BusinessQrCodeToken == businessQrCodeToken &&
-            q.Status == AppConstants.Queues.Status.Waiting)
-        .Select(q => q.Status)
-        .CountAsync();
-
-        // SELECT COUNT("Status") FROM "QueueEntries" WHERE "BusinessQrCodeToken" = '' AND "Status" = 'served' AND "ServedAt" = CURRENT_DATE;
-        int clientsServedToday = await context.Queues
-        .Where(q =>
-            q.BusinessQrCodeToken == businessQrCodeToken &&
-            q.Status == AppConstants.Queues.Status.Served &&
-            q.ServedAt == dateTimeNow)
-        .Select(q => q.Status)
-        .CountAsync();
-
-        // var averageWaitMinutes = context.Queues.FromSql($"SELECT ROUND(AVG(EstimatedWaitTime), 1) FROM QueueEntries WHERE BusinessQrCodeToken = {businessQrCodeToken}");
-        var averageWaitMinutes = Math.Round(context.Queues
-            .Where(q => q.BusinessQrCodeToken == businessQrCodeToken)
-            .Select(q => q.EstimatedWaitTime)
-            .Average(), 1);
-
-        var response = new GetBusinessLiveKpisResponse
-        {
-            BusinessQrCodeToken = business.QrCodeToken,
-            UpdatedAt = business.UpdatedAt,
-            ClientsWaiting = clientsWaiting,
-            ClientsServedToday = clientsServedToday,
-            AverageWaitMinutes = averageWaitMinutes,
-            QueueOpenSince = dateTimeNow
-        };
-
-        logger.LogInformation("[LOG] KPIS : {@0}", response);
-
-        return response;
     }
 }
